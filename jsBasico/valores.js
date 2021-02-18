a19b0605646116cd96642f70c939924ef20921d6
ccrodriguezc <ccrodriguezcorreal@gmail.com>
2021-02-18 16:33:32 -0500

    fourth commit

---
+++ jsBasico/valores.js
@@ -0,0 +1,12 @@
+40
+
+"Cristian"
+
+true
+false
+
+null
+undefined
+
+[1,2,3]
+{nombre: "Diego"}
---
+++ jsBasico/variables.js
@@ -0,0 +1,21 @@
+var nombre = "Cristian"
+
+// declarar Variable
+
+var edad;
+
+//inicializar
+
+edad = 30;
+
+edad
+
+var elemntos = ["computador", "Calular"];
+
+var persona = {
+    nombre: "Diego", 
+    edad: 34
+}
+
+persona;
+
